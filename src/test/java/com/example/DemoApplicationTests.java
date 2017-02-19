4af21604e3580190708927fee3894af3a80bbda7
git <sunjf@biomarker.com.cn>
2017-02-19 13:58:58 +0800

    test

--- src/test/java/com/example/DemoApplicationTests.java
+++ src/test/java/com/example/DemoApplicationTests.java
@@ -1,7 +1,12 @@
 package com.example;
 
+import com.example.entity.User;
+import com.example.repository.UserResitory;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.security.SecurityProperties;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
@@ -11,6 +16,15 @@ public class DemoApplicationTests {
 
 	@Test
 	public void contextLoads() {
+	}
+
+	@Autowired
+	UserResitory userResitory;
+
+	@Test
+	public void findByUsername() {
+		User user =  userResitory.findByUsername("123");
+		System.out.println(user.getId() + " | " + user.getUsername() + " | " + user.getPassword());
 	}
 
 }
